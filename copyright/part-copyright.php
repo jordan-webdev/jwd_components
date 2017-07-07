d69c55727255a677e4a9965b900a88ab7d6e1e3f
jordan-webdev <jordan.trombone@gmail.com>
2017-07-07 15:57:38 -0400

    Create part-copyright.php

---
+++ copyright/part-copyright.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Copyright
+ * Copyright text with year, Dolce Media Link
+ */
+?>
+
+<div class="copyright__wrapper padding-site color-tertiary--bg">
+  <p class="copyright container-site">
+    <span class="copyright__site">
+      Copyright &copy; <?php Date('Y'); ?>. All Rights Reserved <?php bloginfo('title'); ?>
+    </span>
+    <span class="copyright__dolce">
+      Designed and developed by
+      <a href="http://www.dolcemedia.ca/" rel="noopener noreferrer" target="_blank">Dolce Media Group</a>
+    </span>
+  </p>
+</div>
