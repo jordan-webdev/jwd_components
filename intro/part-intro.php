11e4918f0d15fe122d3e236813d1241abea190ef
jordan-webdev <jordan.trombone@gmail.com>
2017-07-07 10:49:58 -0400

    Update part-intro.php

--- intro/part-intro.php
+++ intro/part-intro.php
@@ -6,9 +6,12 @@
 $title = get_field('intro_title');
 $text = get_field('intro_text');
 ?>
-<header class="intro margin-site color-secondary">
-  <h2 class="intro__title"><?php echo esc_html($title); ?></h2>
-  <?php if ($text): ?>
-    <p class="intro__text"><?php echo esc_html($text); ?></p>
-  <?php endif; ?>
-</header>
+
+<div class="intro__wrapper padding-site">
+  <header class="intro container-site color-secondary">
+    <h2 class="intro__title"><?php echo esc_html($title); ?></h2>
+    <?php if ($text): ?>
+      <p class="intro__text"><?php echo esc_html($text); ?></p>
+    <?php endif; ?>
+  </header>
+</div>
