8a8f49ec94c4aaff0a5c3f2dceb070e38c305d4a
jordan-webdev <jordan.trombone@gmail.com>
2017-07-11 15:23:33 -0400

    Update part-breadcrumb.php

--- breadcrumb/part-breadcrumb.php
+++ breadcrumb/part-breadcrumb.php
@@ -3,10 +3,12 @@
  * Breadcrumb. Includes Page title and breadcrumb
  * Requires Breadcrumb NavXT https://en-ca.wordpress.org/plugins/breadcrumb-navxt/
  */
+
+$title = is_category() ? strtoupper(single_cat_title('', false)) : strtoupper(get_the_title());
 ?>
 <div class="padding-site breadcrumb__wrapper" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/breadcrumb-bg.jpg)">
   <header class="breadcrumb color-white container-site">
-    <h1 class="breadcrumb__title"><?php echo strtoupper(esc_html(get_the_title())); ?></h1>
+    <h1 class="breadcrumb__title"><?php echo $title; ?></h1>
     <nav class="breadcrumb__navigation"><?php bcn_display($return = false, $linked = true, $reverse = false, $force = false); ?></nav>
   </header>
 </div>
