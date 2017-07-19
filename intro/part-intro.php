0b8c8abdc02068f157fe4ca0f5b54b027ae9c1f9
jordan-webdev <jordan.trombone@gmail.com>
2017-07-19 11:10:27 -0400

    Update part-intro.php

--- intro/part-intro.php
+++ intro/part-intro.php
@@ -3,10 +3,17 @@
  * Intro blurb for a page. Includes title and text
  */
 
+$archive = is_archive();
+
+//ACF or existing title
 $title = is_null($title) || empty($title) ? get_field('intro_title') : $title;
-$title = $title ? $title : single_cat_title('', false);
+//Archive page (including category)
+$title = $archive ? str_replace(array("Archives: ", "Category: "), "", get_the_archive_title()) : $title;
+
+//ACF or existing text
 $text = isset($text) ? $text : get_field('intro_text');
-$text = $text ? $text : str_replace(array('<p>', '</p>'), '', category_description());
+//Archive page (including category)
+$text = $archive ? str_replace(array('<p>', '</p>'), '', get_the_archive_description()) : $text;
 ?>
 
 <div class="intro__wrapper padding-site">
