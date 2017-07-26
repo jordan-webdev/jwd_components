4d9eb20d0b26ff72777d9d2d518e65889b909190
jordan-webdev <jordan.trombone@gmail.com>
2017-07-26 16:30:24 -0400

    Create part-product-headers.php

---
+++ woocommerce-single/part-product-headers.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Template part for product headers, which are anchors to sections of the page
+ */
+?>
+
+<div id="options" class="product-chart__header-links none">
+  <a class="product-chart__header-link btn btn--secondary flex-grow-1 text-center" href="#configure" data-offset="-100">CONFIGURE</a>
+  <a class="product-chart__header-link btn btn--secondary flex-grow-1 text-center" href="#specifications" data-offset="-100">SPECIFICATIONS</a>
+  <a class="product-chart__header-link btn btn--secondary flex-grow-1 text-center" href="#overview" data-offset="-100">OVERVIEW</a>
+  <a class="product-chart__header-link btn btn--secondary flex-grow-1 text-center" href="#pdf" data-offset="-100">DOWNLOAD PDF</a>
+</div>
