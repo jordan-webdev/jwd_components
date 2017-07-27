5eda3e398897f0022f3c8c1a3e9db938420553ff
jordan-webdev <jordan.trombone@gmail.com>
2017-07-27 11:43:14 -0400

    Update news_specials_post_type.php

--- news-media/news_specials_post_type.php
+++ news-media/news_specials_post_type.php
@@ -1,22 +1,20 @@
-<?php
-//Include in functions.php
-
 // News & Specials Custom Post Type
-function custom_news_specials() {
-$labels = array(
-    'name' => __( 'News & Specials'),
-    'singular_name' => __( 'News & Specials'),
-    'all_items' => __( 'All News & Specials'),
-    'add_new' => __( 'Add News & Specials'),
-    'add_new_item' => __( 'Add News & Specials'),
-    'edit_item' => __( 'Edit News & Specials'),
-    'new_item' => __( 'New News & Specials'),
-    'view_item' => __( 'View News & Specials'),
-    'not_found' => __( 'No News & Specials found'),
-    'not_found_in_trash' => __( 'No News & Specials found in Trash'),
+function custom_blog_posts()
+{
+    $labels = array(
+    'name' => __('Blog Posts'),
+    'singular_name' => __('Blog Post'),
+    'all_items' => __('All Blog Posts'),
+    'add_new' => __('Add new Blog Post'),
+    'add_new_item' => __('Add new Blog Post'),
+    'edit_item' => __('Edit Blog Post'),
+    'new_item' => __('New Blog Post'),
+    'view_item' => __('View Blog Post'),
+    'not_found' => __('No Blog Posts found'),
+    'not_found_in_trash' => __('No Blog Posts found in Trash'),
 );
- 
-$supports = array(
+
+    $supports = array(
     'title',
     'editor',
     'author',
@@ -27,21 +25,21 @@ $supports = array(
     'page-attributes',
     'post-formats',
 );
- 
-$args = array(
+
+    $args = array(
     'labels' => $labels,
     'public' => true,
     'publicly_queriable' => true,  // you should be able to query it
-	'capability_type'     => 'page',
+    'capability_type'     => 'page',
     'show_ui' => true,  // you should be able to edit it in wp-admin
     'exclude_from_search' => false,  // you should exclude it from search results
     'show_in_nav_menus' => true,  // you shouldn't be able to add it to menus
     'has_archive' => false,  // it shouldn't have archive page
     'rewrite' => false,  // it shouldn't have rewrite rules
     'supports' => $supports,
-	'taxonomies' => array( 'category' ),
+    'menu_icon' => 'dashicons-welcome-write-blog',
+    'taxonomies' => array( 'category' ),
 );
-register_post_type('news_specials', $args);
-
+    register_post_type('blog_posts', $args);
 }
-add_action( 'init', 'custom_news_specials', 0 );
+add_action('init', 'custom_blog_posts', 0);
