979c8511be55a52b0f4be9a0643e984429f4e509
jordan-webdev <jordan.trombone@gmail.com>
2017-07-27 10:47:09 -0400

    Update get_excerpt.php

--- functions/get_excerpt.php
+++ functions/get_excerpt.php
@@ -1,10 +1,11 @@
 //Get excerpt function
 function get_excerpt($limit)
 {
+    global $post;
     $excerpt = get_the_content();
     $excerpt = strip_shortcodes($excerpt);
     $excerpt = strip_tags($excerpt);
     $excerpt = substr($excerpt, 0, $limit);
-    $excerpt = $excerpt.'… <a class="read-more-link main-colour sub-header" href="'. get_permalink($post->ID) . '">' . 'Read More' . '<span class="fa fa-chevron-right aria-hidden="true"></span></a>';
+    $excerpt = $excerpt.'… <a class="read-more-link color-primary" href="'. get_permalink($post->ID) . '">' . 'Read More' . '<span class="fa fa-chevron-right aria-hidden="true"></span></a>';
     return $excerpt;
 }
