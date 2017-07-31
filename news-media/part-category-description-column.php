51812ca70db328d0da4f9f3f43c3819f52b5f64a
jordan-webdev <jordan.trombone@gmail.com>
2017-07-31 14:58:46 -0400

    Update part-category-description-column.php

--- news-media/part-category-description-column.php
+++ news-media/part-category-description-column.php
@@ -4,19 +4,15 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package JBEC
+ * @package jwd
  */
 
-$args = array(
-	'post_type'      => 'blog_posts',
-	'posts_per_page' => -1,
-);
+$blog_category = get_the_terms($post->ID, 'blog')[0];
+$blog_category_slug = $blog_category->slug;
+$single_slug = $post->post_name;
 
-$the_query = new WP_Query( $args );
 ?>
 
-<?php  if ( $the_query->have_posts() ) :
-		while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 		<?php
 			$external = get_field('external_link');
 			$youtube = get_field('youtube_link');
@@ -24,78 +20,109 @@ $the_query = new WP_Query( $args );
 			$preview = get_field('preview_image');
 		?>
 
-		<article class="blog-item-wrapper clear <?php echo get_the_category()[0]->category_nicename; ?>">
-			<div class="blog-preview-item">
-				<?php if ($external) : ?>
-					<a href="<?php echo $external ?>">
-						<?php
-							if ($preview) {
-								echo '<img class="news-thumb-preview" src="' . $preview['url'] . '" alt="' . $preview['alt'] . '" />';
-							}
-						?>
-					</a>
-				<?php elseif ($youtube) :?>
-					<a href="<?php echo $youtube ?>">
-						<?php
-							if ($preview) {
-								echo '<img class="news-thumb-preview" src="' . $preview['url'] . '" alt="' . $preview['alt'] . '" />';
-							}
-						?>
-					</a>
-				<?php elseif ($pdf) : ?>
-					<a href="<?php echo $pdf['url'] ?>">
-						<?php
-							if ($preview) {
-								echo '<img class="news-thumb-preview" src="' . $preview['url'] . '" alt="' . $preview['alt'] . '" />';
-							}
-						?>
-					</a>
-				<?php else: ?>
-					<a href="<?php echo get_the_permalink(); ?>">
-						<?php
-							$preview = get_field('preview_image');
-							if ($preview)
-								echo '<img class="news-thumb-preview" src="' . $preview['url'] . '" alt="' . $preview['alt'] . '" />';
-						?>
-					</a>
-				<?php endif; ?>
-			</div>
-			<div class="blog-preview-item">
-				<div class="news-preview-date-wrapper">
-					<span class="fa fa-calendar-o color-primary" aria-hidden="true"></span>
-					<span class="blog-preview-author"><?php echo get_the_date('F j, Y'); ?></span>
-				</div>
+    <div class="color-grey--bg pad-b-15 pad-l-15 pad-r-15 blog-item-padding-wrapper">
+  		<article class="blog-item-wrapper clear <?php echo $blog_category_slug; ?> all">
+  			<div class="blog-preview-item">
+  				<?php if ($external) : ?>
+  					<a href="<?php echo $external ?>">
+  						<?php
+  							if ($preview) {
+  								echo '<img class="news-thumb-preview" src="' . $preview['url'] . '" alt="' . $preview['alt'] . '" />';
+  							}
+  						?>
+  					</a>
+  				<?php elseif ($youtube) :?>
+  					<a href="<?php echo $youtube ?>">
+  						<?php
+  							if ($preview) {
+  								echo '<img class="news-thumb-preview" src="' . $preview['url'] . '" alt="' . $preview['alt'] . '" />';
+  							}
+  						?>
+  					</a>
+  				<?php elseif ($pdf) : ?>
+  					<a href="<?php echo $pdf['url'] ?>">
+  						<?php
+  							if ($preview) {
+  								echo '<img class="news-thumb-preview" src="' . $preview['url'] . '" alt="' . $preview['alt'] . '" />';
+  							}
+  						?>
+  					</a>
+  				<?php else: ?>
+            <!-- Preview image for internal stories -->
+  					<a
+              class="js-blog-ajax-link"
+              href="<?php echo get_the_permalink(); ?>"
+              data-slug="<?php echo $blog_category_slug; ?>"
+              data-single-slug="<?php echo $single_slug; ?>"
+            >
+  						<?php
+  							$preview = get_field('preview_image');
+  							if ($preview)
+  								echo '<img class="news-thumb-preview" src="' . $preview['url'] . '" alt="' . $preview['alt'] . '" />';
+  						?>
+  					</a>
+  				<?php endif; ?>
+  			</div>
+  			<div class="blog-preview-item">
+  				<div class="news-preview-date-wrapper">
+            <span class="fa fa-calendar-o color-primary" aria-hidden="true"></span>
+  					<span class="blog-preview-author"><?php echo get_the_date('F j, Y'); ?></span>
+  				</div>
 
-				<?php if ($external): ?>
-					<a href="<?php echo $external ?>">
-						<h3 class="sub-header news-preview-title"><?php echo get_the_title(); ?></h3>
-					</a>
-					<p class="body-font news-media-excerpt"><?php echo get_the_content(); ?></p>
-					<a href="<?php echo $external ?>">Go to Article</a>
-				<?php elseif ($youtube): ?>
-					<a href="<?php echo $youtube ?>">
-						<h3 class="sub-header news-preview-title"><?php echo get_the_title(); ?></h3>
-					</a>
-					<p class="body-font news-media-excerpt"><?php echo get_the_content(); ?></p>
-					<a href="<?php echo $youtube ?>">Watch Video</a>
-				<?php elseif ($pdf): ?>
-					<a href="<?php echo $pdf['url'] ?>">
-						<h3 class="sub-header news-preview-title"><?php echo get_the_title(); ?></h3>
-					</a>
-					<p class="body-font news-media-excerpt"><?php echo get_the_content(); ?></p>
-					<a href="<?php echo $pdf['url'] ?>">View PDF</a>
-				<?php else: ?>
-					<a href="<?php echo get_the_permalink(); ?>">
-						<h3 class="sub-header news-preview-title"><?php echo get_the_title(); ?></h3>
-					</a>
-					<p class="body-font news-media-excerpt"><?php echo get_excerpt(150); ?></p>
-				<?php endif; ?>
+  				<?php if ($external): ?>
+            <!-- External Article -->
+  					<a href="<?php echo $external ?>" target="_blank" rel="noopener noreferrer">
+  						<h3
+                class="color-secondary news-preview-title"
+                data-slug="<?php echo $blog_category_slug; ?>"
+                data-single-slug="<?php echo $single_slug; ?>"
+              >
+                <?php echo get_the_title(); ?>
+              </h3>
+  					</a>
+  					<p class="body-font news-media-excerpt"><?php echo get_the_content(); ?></p>
+  					<a href="<?php echo esc_url($external); ?>" target="_blank" rel="noopener noreferrer">Go to Article</a>
+  				<?php elseif ($youtube): ?>
+            <!-- YouTube -->
+  					<a href="<?php echo $youtube ?>" target="_blank" rel="noopener noreferrer">
+              <h3
+                class="color-secondary news-preview-title"
+                data-slug="<?php echo $blog_category_slug; ?>"
+                data-single-slug="<?php echo $single_slug; ?>"
+              >
+                <?php echo get_the_title(); ?>
+              </h3>
+  					</a>
+  					<p class="body-font news-media-excerpt"><?php echo get_the_content(); ?></p>
+  					<a href="<?php echo esc_url($youtube); ?>" target="_blank" rel="noopener noreferrer">Watch Video</a>
+  				<?php elseif ($pdf): ?>
+            <!-- PDF -->
+  					<a href="<?php echo $pdf['url'] ?>" target="_blank" rel="noopener noreferrer">
+              <h3
+                class="color-secondary news-preview-title"
+                data-slug="<?php echo $blog_category_slug; ?>"
+                data-single-slug="<?php echo $single_slug; ?>"
+              >
+                <?php echo get_the_title(); ?>
+              </h3>
+  					</a>
+  					<p class="body-font news-media-excerpt"><?php echo get_the_content(); ?></p>
+  					<a href="<?php echo esc_url($pdf['url']); ?>" target="_blank" rel="noopener noreferrer">View PDF</a>
+  				<?php else: ?>
+            <!-- Internal Story -->
+  					<a
+              href="<?php echo get_the_permalink(); ?>"
+              class="js-blog-ajax-link"
+              data-slug="<?php echo $blog_category_slug; ?>"
+              data-single-slug="<?php echo $single_slug; ?>"
+            >
+              <h3 class="color-secondary news-preview-title">
+                <?php echo get_the_title(); ?>
+              </h3>
+  					</a>
+  					<p class="body-font news-media-excerpt"><?php echo get_excerpt(150, $blog_category_slug, $single_slug); ?></p>
+  				<?php endif; ?>
 
-			</div>
-		</article>
-	<?php
-		endwhile;
-	endif;
-	/* Restore original Post Data */
-	wp_reset_postdata();
-?>
+  			</div>
+  		</article>
+    </div>
