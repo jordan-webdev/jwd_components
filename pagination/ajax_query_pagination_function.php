3ca31ae8ad74633c5aecd40aa55d4be76b1a78b7
jordan-webdev <jordan.trombone@gmail.com>
2017-08-01 12:56:56 -0400

    Create ajax_query_pagination_function.php

---
+++ pagination/ajax_query_pagination_function.php
@@ -0,0 +1,23 @@
+function ajax_query_pagination($wp_query, $tmp_query){
+  /* PAGINATION */
+  $big = 999999999; // need an unlikely integer
+
+  $args = array(
+      'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+      'format' => '?paged=%#%',
+      'current' => max(1, get_query_var('paged')),
+      'total' => $wp_query->max_num_pages,
+      'prev_text'          => __('<span>&laquo;</span><span class="screen-reader-text">Previous</span>'),
+      'next_text'          => __('<span>&raquo;</span><span class="screen-reader-text">Next</span>'),
+      'mid_size' => 1,
+  );
+  ?>
+  <nav class="pagination-wrapper flex flex-wrap justify-center">
+    <?php echo paginate_links($args); ?>
+  </nav>
+<?php
+  wp_reset_postdata();
+
+  $wp_query = null;
+  $wp_query = $tmp_query;
+}
