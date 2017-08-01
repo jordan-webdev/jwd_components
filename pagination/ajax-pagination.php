d2e99dc756925804aba4f86f0387b2d4dff0b1d3
jordan-webdev <jordan.trombone@gmail.com>
2017-08-01 09:18:05 -0400

    Update ajax-pagination.php

--- pagination/ajax-pagination.php
+++ pagination/ajax-pagination.php
@@ -2,3 +2,12 @@
 <div class="ajax-pagination">
   <?php ajax_archive_pagination(); ?>
 </div>
+
+<!-- Results -->
+<div class="js-ajax-content-container">
+  <div class="js-ajax-results">
+    <?php while ( have_posts() ) : the_post(); ?>
+      <?php get_template_part('template-parts/part', 'faq'); ?>
+    <?php endwhile; ?>
+  </div>
+</div>
