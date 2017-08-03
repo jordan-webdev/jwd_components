61602b90d888a33e7a5ae74d379944e272a60561
jordan-webdev <jordan.trombone@gmail.com>
2017-08-03 15:01:08 -0400

    Update faq_cpt.php

--- tax-accordions/faq_cpt.php
+++ tax-accordions/faq_cpt.php
@@ -1,32 +1,42 @@
-function cptui_register_my_cpts_faq() {
-	$labels = array(
-		"name" => __( 'FAQs', 'jwd' ),
-		"singular_name" => __( 'FAQ', 'jwd' ),
-		);
+// Glossary Term Custom Post Type
+ function register_glossary_term() {
 
-	$args = array(
-		"label" => __( 'FAQs', 'jwd' ),
-		"labels" => $labels,
-		"description" => "",
-		"public" => true,
-		"publicly_queryable" => true,
-		"show_ui" => true,
-		"show_in_rest" => false,
-		"rest_base" => "",
-		"has_archive" => false,
-		"show_in_menu" => true,
-				"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => array( "slug" => "faq", "with_front" => true ),
-		"query_var" => true,
-		"menu_icon" => "dashicons-format-status",
-		"supports" => array( "title", "editor", "thumbnail" ),
-		"taxonomies" => array( "category" ),
-			);
-	register_post_type( "faq", $args );
+ $labels = array(
+ 'name' => __( 'Glossary Terms'),
+ 'singular_name' => __( 'Glossary Term'),
+ 'all_items' => __( 'All Glossary Terms'),
+ 'add_new' => __( 'Add New Glossary Term'),
+ 'add_new_item' => __( 'Add New Glossary Term'),
+ 'edit_item' => __( 'Edit Glossary Term'),
+ 'new_item' => __( 'New Glossary Term'),
+ 'view_item' => __( 'View Glossary Term'),
+ 'not_found' => __( 'No Glossary Terms found'),
+ 'not_found_in_trash' => __( 'No Glossary Terms found in Trash'),
+ );
 
-// End of cptui_register_my_cpts_faq()
-}
-add_action( 'init', 'cptui_register_my_cpts_faq' );
+ $args = array(
+ 'label'               => __( 'Glossary Terms' ),
+ 'description'         => __( ''),
+ 'labels'              => $labels,
+ 'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+ 'hierarchical'        => false,
+ 'public'              => true,
+ 'show_ui'             => true,
+ 'show_in_menu'        => true,
+ 'show_in_nav_menus'   => true,
+ 'show_in_admin_bar'   => true,
+ 'can_export'          => true,
+ 'has_archive'         => true,
+ 'exclude_from_search' => false,
+ 'publicly_queryable'  => true,
+ 'capability_type'     => 'page',
+ 'menu_icon'           => 'dashicons-editor-help',
+ // This is where we add taxonomies to our CPT
+ 'taxonomies'          => array( 'glossary_category' ),
+ );
+
+ register_post_type( 'glossary_term', $args );
+
+ }
+
+ add_action( 'init', 'register_glossary_term', 0 );
