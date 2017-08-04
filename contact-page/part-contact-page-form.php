657ee9cd73db8c2594e479d89473ac0a8b7656b7
jordan-webdev <jordan.trombone@gmail.com>
2017-08-04 17:04:01 -0400

    Create part-contact-page-form.php

---
+++ contact-page/part-contact-page-form.php
@@ -0,0 +1,4 @@
+<div class="contact-page__form contact-page__col">
+  <h2 class="contact-page__title color-secondary ">HOW MAY WE HELP?</h2>
+  <?php echo do_shortcode('[contact-form-7 id="1087" title="HOW MAY WE HELP?"]'); ?>
+</div>
