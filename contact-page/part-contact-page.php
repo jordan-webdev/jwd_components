a037b1d3f26b071b3635044b4193a461c9fc0f08
jordan-webdev <jordan.trombone@gmail.com>
2017-08-04 17:02:46 -0400

    Create part-contact-page.php

---
+++ contact-page/part-contact-page.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Template part for a contact page
+ */
+?>
+
+<div class="contact-page flex column">
+  <?php get_template_part('template-parts/part', 'contact-page-info'); ?>
+  <?php get_template_part('template-parts/part', 'contact-page-form'); ?>
+</div>
