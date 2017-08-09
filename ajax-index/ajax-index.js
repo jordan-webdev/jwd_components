7e1d040236ce85588a8dfcb5bcbbc692cd24fc2b
jordan-webdev <jordan.trombone@gmail.com>
2017-08-09 11:58:34 -0400

    Update ajax-index.js

--- ajax-index/ajax-index.js
+++ ajax-index/ajax-index.js
@@ -2,49 +2,84 @@
 
   /* ********** *CATEGORY SELECT + DESCRIPTION ******************* */
 
+  //Update the hrefs for filter links (if any) for the first time
+  updateFilterHrefs(true);
+
   // Ajax load content on link click
-  $('body').on('click', '#categories a, .js-blog-ajax-link, .pagination-wrapper a', function(e){
+  $('body').on('click', '#categories a, .js-blog-ajax-link, .js-ajax-link, .pagination-wrapper a', function(e) {
+
+    if ($(this).parent('li').hasClass('not-clickable')){
+      return false;
+    }
+
+    var clickedLink = $(this);
     var slug = $(this).attr('data-slug');
     var href = $(this).attr('href');
+    href = href.replace(" ", "%20");
     var target = $('.js-ajax-content-container');
     var singleSlug = $(this).attr('data-single-slug');
 
-    //Results
+    // Prevent clicking until load
+    $('.drop-down--filters__sub-item, .drop-down__all').addClass('not-clickable');
+
+    // Results
     target.fadeOut();
-    target.load(href + ' .js-category-results', function(){
+    // Show Ajax loader
+    $('.two-column-item__ajax-loader').addClass('active');
+    target.load(href + ' .js-category-results', function(data) {
+      // Hide Ajax loader
+      $('.two-column-item__ajax-loader').removeClass('active');
       //var newURL = singleSlug ? '/new_era/blog/' + slug + '/' + singleSlug:  '/new_era/blog/' + slug + '/';
       var getUrl = window.location;
-      var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+      var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
       //var newURL = href.replace(baseUrl, '');
       var newURL = href.replace('https://dolcedev.com', '');
-      window.history.replaceState( {} , '', newURL );
+      window.history.replaceState({}, '', newURL);
+
+      // Colour toggle the selected link for filters (if any)
+      clickedLink.prev('.js-filter-box').toggleClass('is-active');
+
+      //Remove the colour toggle for all if all was not selected, or remove it from every other one if all was selected
+      if ( ! (clickedLink.hasClass('js-filter-link-all') || clickedLink.hasClass('js-pagination-link')) ) {
+        $('.js-filter-box-all.is-active').removeClass('is-active');
+      }else{
+        $('.js-filter-box.is-active').not('.js-filter-box-all').removeClass('is-active');
+      }
+
+      //Update the hrefs for filter links (if any)
+      updateFilterHrefs(false);
 
       //Give phones some extra time to load
-      let mq = window.matchMedia( "(max-width: 450px)" );
-      if (mq.matches){
-        setTimeout(function(){
+      let mq = window.matchMedia("(max-width: 450px)");
+      if (mq.matches) {
+        setTimeout(function() {
           target.fadeIn('slow');
         }, 200);
-      }else{
+      } else {
         target.fadeIn('slow');
       }
+
+      // Allow clicking again
+      $('.drop-down--filters__sub-item.not-clickable, .drop-down__all.not-clickable').removeClass('not-clickable');
 
     });
 
     //Pagination
     var paginationTarget = $('.ajax-pagination');
     paginationTarget.fadeOut();
-    paginationTarget.load(href + ' .js-ajax-pagination', function(){
+    paginationTarget.load(href + ' .js-ajax-pagination', function() {
       paginationTarget.fadeIn();
     });
 
     //Document title
-    $('title').load(href + ' title', function(data){
-       document.title = $(this).text();
+    $('title').load(href + ' title', function(data) {
+      document.title = $(this).text();
     })
 
     //Scroll page
-    $('html,body').animate({ scrollTop: ($('#category-wrapper').offset().top - 150) }, 'slow');
+    $('html,body').animate({
+      scrollTop: ($('#category-wrapper').offset().top - 150)
+    }, 'slow');
 
     return false;
   });
@@ -55,7 +90,7 @@
     $(this).addClass('color-primary');
   });
 
-  $('body').on('click', '.js-blog-ajax-link', function(){
+  $('body').on('click', '.js-ajax-link', function() {
     var slug = $(this).attr('data-slug');
     $('#categories span.color-primary').removeClass('color-primary');
     $('#categories span[data-slug="' + slug + '"]').addClass('color-primary');
@@ -64,11 +99,70 @@
   //Categories dropdown
   var mq = window.matchMedia("(max-width: 850px)");
   if (mq.matches) {
-    $('#categories-wrapper').slideUp(1, function(){});
-    $('.js-categories-wrapper-toggle, .js-category-selector-link').on('click', function(){
+    $('#categories-wrapper').slideUp(1, function() {});
+    $('.js-categories-wrapper-toggle, .js-category-selector-link').on('click', function() {
       $('#categories-wrapper').slideToggle();
       $('.js-categories-wrapper-toggle').toggleClass('active');
     });
+  }
+
+  function updateFilterHrefs(firstLoad) {
+
+    $('.js-filter-link').each(function() {
+      var value = $(this).attr('data-filter-value').replace(" ", "%20");
+      var key = $(this).attr('data-filter-key');
+      var currentHref = window.location.href;
+      var delimiter = currentHref.indexOf('?') > -1 ? "&" : "?";
+
+      if (firstLoad) {
+        var updatedHref = currentHref.split("?")[0];
+        updatedHref = updatedHref + "?" + key + "=" + value;
+        $(this).attr('href', updatedHref);
+      } else {
+        var updatedHref = currentHref + delimiter + key + "=" + value;
+        updatedHref = updatedHref.replace(/\/page\/[0-9]\//g, "");
+        updatedHref = updatedHref.replace(/%5B[0-9]%5D/g, "[]");
+        $(this).attr('href', updatedHref);
+      }
+    }).promise().done(function() {
+      //Remove the key and value of active filters from their hrefs, so that they get deselected on click
+      $('.js-filter-box.is-active').next('.js-filter-link').each(function() {
+        var key = $(this).attr('data-filter-key');
+        var value = $(this).attr('data-filter-value');
+        var value2 = value.replace(" ", "%20");
+        var value3 = value.replace(" ", "+");
+        var href = $(this).attr('href');
+        updatedHref = href.replaceAll("&" + key + "=" + value, "").replaceAll("&" + key + "=" + value2, "").replaceAll("&" + key + "=" + value3, "");
+        updatedHref = updatedHref.replaceAll(key + "=" + value, "").replaceAll(key + "=" + value2, "").replaceAll(key + "=" + value3, "");
+        $(this).attr('href', updatedHref)
+      });
+    });
+
+    //Removing any improperly encoded entities and duplicates from the pagination links
+    tidyPaginationLinks();
+  }
+
+  function tidyPaginationLinks() {
+    $('a.page-numbers').each(function() {
+      var href = $(this).attr('href');
+      var hrefBeforeGETQueries = href.split("?")[0];
+      var GETQueriesString = href.split("?")[1];
+      GETQueriesString = GETQueriesString.replace(/#038;/g, "&");
+      var GETQueriesArray = GETQueriesString.split("&");
+      //console.log(GETQueriesArray);
+      var uniqueGETQueriesArray = array_unique(GETQueriesArray);
+      //console.log(uniqueGETQueriesArray);
+      var updatedHref = hrefBeforeGETQueries + "?";
+
+      var finalIteration = Object.keys(uniqueGETQueriesArray).length - 1;
+      $.each(uniqueGETQueriesArray, function(index, value){
+        updatedHref = index == finalIteration ? updatedHref + value : updatedHref + value + "&";
+      })
+      //var updatedHref = href.replace(/#038;/g, "&");
+      updatedHref = updatedHref.replace(/%5B[0-9]%5D/g, "[]");
+      $(this).attr('href', updatedHref);
+
+    })
   }
 
 })(jQuery)
