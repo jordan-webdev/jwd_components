2116bfa1d43a9f92e376df39e26d51cfbeb1bcda
jordan-webdev <jordan.trombone@gmail.com>
2017-08-23 17:17:55 -0400

    Create glossary_category_taxonomy.php

---
+++ tax-accordions/glossary_category_taxonomy.php
@@ -0,0 +1,31 @@
+// Add the "glossary_category" taxonomy
+function create_glossary_category_taxonomy() {
+
+  $labels = array(
+    'name' => _x( 'Glossary', 'taxonomy general name' ),
+    'singular_name' => _x( 'Glossary Category', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Glossary Categories' ),
+    'all_items' => __( 'All Glossary Categories' ),
+    'parent_item' => __( 'Parent Glossary Category' ),
+    'parent_item_colon' => __( 'Parent Glossary Category:' ),
+    'edit_item' => __( 'Edit Glossary Category' ),
+    'update_item' => __( 'Update Glossary Category' ),
+    'add_new_item' => __( 'Add New Glossary Category' ),
+    'new_item_name' => __( 'New Glossary category Name' ),
+    'menu_name' => __( 'Glossary Categories' ),
+  );
+
+  register_taxonomy(
+    'glossary_category',
+    'glossary_term',
+    array(
+      'hierarchical' => true,
+      'labels' => $labels,
+      'show_ui' => true,
+      'show_admin_column' => true,
+      'query_var' => true,
+      'rewrite' => array( 'slug' => 'blog' ),
+    ));
+
+}
+add_action( 'init', 'create_glossary_category_taxonomy', 0 );
