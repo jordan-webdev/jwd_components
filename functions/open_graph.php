618c47b1fa79abefda33da02d0f2f4d4d0147e21
jordan-webdev <jordan.trombone@gmail.com>
2017-08-24 17:06:50 -0400

    Create open_graph.php

---
+++ functions/open_graph.php
@@ -0,0 +1,24 @@
+//Adding the Open Graph in the Language Attributes
+function add_opengraph_doctype( $output ) {
+        return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
+    }
+add_filter('language_attributes', 'add_opengraph_doctype');
+
+//Lets add Open Graph Meta Info
+
+function insert_fb_in_head() {
+    global $post;
+    if ( !is_singular('blog_posts')){
+      //if it is not a post or a page
+      return;
+    }
+    echo '<meta property="og:title" content="' . get_the_title() . '"/>';
+    echo '<meta property="og:type" content="article"/>';
+    echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+    echo '<meta property="og:site_name" content="' .get_bloginfo('title'). '"/>';
+    if(has_post_thumbnail( $post->ID )) {
+        $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+        echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+    }
+}
+add_action( 'wp_head', 'insert_fb_in_head', 5 );
