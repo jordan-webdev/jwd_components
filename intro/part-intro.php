e9d9d28ee761701d912d7d8e5eeba4601b9363eb
jordan-webdev <jordan.trombone@gmail.com>
2017-08-24 13:42:14 -0400

    Update part-intro.php

--- intro/part-intro.php
+++ intro/part-intro.php
@@ -2,25 +2,31 @@
 /*
  * Intro blurb for a page. Includes title and text
  */
-
 $archive = is_archive();
-
 //ACF or existing title
 $title = is_null($title) || empty($title) ? get_field('intro_title') : $title;
 //Archive page (including category)
-$title = $archive ? str_replace(array("Archives: ", "Category: "), "", get_the_archive_title()) : $title;
-
+$title = $archive ? str_replace(array("Archives: ", "Category: ", " All"), "", get_the_archive_title()) : $title;
+$title = is_tax('blog_category') ? "Blog" : $title;
 //ACF or existing text
 $text = isset($text) ? $text : get_field('intro_text');
 //Archive page (including category)
 $text = $archive ? str_replace(array('<p>', '</p>'), '', get_the_archive_description()) : $text;
 ?>
 
-<div class="intro__wrapper padding-site">
-  <header class="intro container-site color-secondary">
-    <h2 class="intro__title"><?php echo esc_html($title); ?></h2>
-    <?php if ($text): ?>
-      <p class="intro__text"><?php echo esc_html($text); ?></p>
-    <?php endif; ?>
-  </header>
-</div>
+<?php if ($title): ?>
+  <div class="intro__wrapper">
+    <header class="intro text-center pad-t-30">
+      <?php if (is_singular("blog_posts")): ?>
+        <!-- H2 for single -->
+        <h2 class="intro__title header mar-b-15"><?php echo esc_html($title); ?></h2>
+        <?php else: ?>
+        <h1 class="intro__title header mar-b-15"><?php echo esc_html($title); ?></h1>
+      <?php endif; ?>
+
+      <?php if ($text): ?>
+        <p class="intro__text color-dark-grey"><?php echo esc_html($text); ?></p>
+      <?php endif; ?>
+    </header>
+  </div>
+<?php endif; ?>
